033b17d138d70738670a4e48f038a7a6b8b24d0b
Jack Schneble <schneble.jack@gmail.com>
2022-07-06 17:43:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Recreating Facebook 
+
+Using Facebook login authentication, Nextjs, and Tailwind 
+
+Install the neccesary project dependencies
+```bash
+npm install
+# or
+yarn add
+```
+Then run 
+
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+     <h1> The Start </h1>
+    </div>
+  )
+}
