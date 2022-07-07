6ef39f0bbca9807b79909ad8ba41bd2a22288182
Jack Schneble <schneble.jack@gmail.com>
2022-07-07 18:58:45 -0400

    header work

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,7 +1,7 @@
 function HeaderIcon({ Icon }) {
     return (
-        <div>
-            <Icon className='h-5'/>
+        <div className='cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-zl active:border-b-2 active:border-blue-500'>
+            <Icon className='h-5 text-center sm:h-7 mx-auto hover:text-blue-500 '/>
         </div>
     );
 }
