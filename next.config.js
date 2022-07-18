e1c5a24cecef2dc624de250d4102a6ebc3c3f6e8
Jack Schneble <schneble.jack@gmail.com>
2022-07-18 11:55:59 -0400

    restarting firebase add, and added hosting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   images: {
     domains: [
+      "scontent-lax3-1.xx.fbcdn.net",
       "links.papareact.com",
       "platform-lookaside.fbsbx.com",
       "firebasestorage.googleapis.com",
+     
     ],
   },
 };
