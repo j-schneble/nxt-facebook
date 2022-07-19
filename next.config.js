955495f0958e0de7f592927725ae18ed6caf9040
Jack Schneble <schneble.jack@gmail.com>
2022-07-19 17:47:43 -0400

    hosting

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ module.exports = {
       "platform-lookaside.fbsbx.com",
       "firebasestorage.googleapis.com",
       "gs://nxtjs-fb.appspot.com",
-      "scontent-mia3-1.xx.fbcdn.net" , 
-      "nxt-facebook.vercel.app"]
+      "scontent-mia3-1.xx.fbcdn.net"]
   },
 };
